f824aef616ed2f1621d1b02e28043c7dffceeb6b
John Granström <granstrom.john@gmail.com>
2017-01-24 18:16:17 +0100

    Added tests

---
+++ test/index.js
@@ -0,0 +1,62 @@
+const { expect } = require('chai');
+const runTest = require('./run-test')
+const path = require('path');
+
+describe('sass-vars-loader', () => {
+  it('should succesfully compile basic variables', () => {
+    return runTest('./scss/basic.scss')
+    .then(({ compiled, stats }) => {
+      expect(compiled).to.be.a('object');
+      expect(compiled).to.have.property('global');
+      expect(compiled.global).to.have.keys(['$a', '$b', '$c', '$d', '$e', '$f']);
+      const source = path.resolve(__dirname, './scss/basic.scss');
+
+      expect(compiled.global.$a.sources).to.have.members([source]);
+      expect(compiled.global.$b.sources).to.have.members([source]);
+      expect(compiled.global.$c.sources).to.have.members([source]);
+      expect(compiled.global.$d.sources).to.have.members([source]);
+      expect(compiled.global.$e.sources).to.have.members([source]);
+      expect(compiled.global.$f.sources).to.have.members([source]);
+
+      expect(stats.compilation.fileDependencies).to.have.members([source]);
+    });
+  });
+
+  it('should succesfully compile nested variables', () => {
+    return runTest('./scss/nested.scss')
+    .then(({ compiled, stats }) => {
+      expect(compiled).to.be.a('object');
+      expect(compiled).to.have.property('global');
+      expect(compiled.global).to.have.keys(['$a', '$b', '$c', '$d', '$e', '$f']);
+      const sourceRoot = path.resolve(__dirname, './scss/nested.scss');
+      const sourceSub1 = path.resolve(__dirname, './scss/sub/sub1.scss');
+      const sourceSub2 = path.resolve(__dirname, './scss/sub/sub2.scss');
+
+      expect(compiled.global.$a.sources).to.have.members([sourceRoot]);
+      expect(compiled.global.$b.sources).to.have.members([sourceRoot]);
+      expect(compiled.global.$c.sources).to.have.members([sourceRoot]);
+      expect(compiled.global.$d.sources).to.have.members([sourceSub1]);
+      expect(compiled.global.$e.sources).to.have.members([sourceSub2]);
+      expect(compiled.global.$f.sources).to.have.members([sourceSub2]);
+
+      expect(stats.compilation.fileDependencies).to.have.members([sourceRoot, sourceSub1, sourceSub2]);
+    });
+  });
+
+  it('should succesfully track dependencies on error', () => {
+    return runTest('./scss/error.scss')
+    .catch(err => {
+      const source = path.resolve(__dirname, './scss/error.scss');
+      expect(err.stats.compilation.fileDependencies).to.have.members([source]);
+    });
+  });
+
+  it('should succesfully track included dependencies on nested error', () => {
+    return runTest('./scss/nested-error.scss')
+    .catch(err => {
+      const sourceRoot = path.resolve(__dirname, './scss/nested-error.scss');
+      const sourceSub = path.resolve(__dirname, './scss/sub/error.scss');
+      expect(err.stats.compilation.fileDependencies).to.have.members([sourceRoot, sourceSub]);
+    });
+  });
+});
---
+++ test/run-test.js
@@ -0,0 +1,41 @@
+const Promise = require('bluebird');
+const webpack = Promise.promisify(require('webpack'));
+const path = require('path');
+
+const pathToLoader = path.resolve(__dirname, "../index.js");
+const pathToTestBundle = path.resolve(__dirname, "./output/test.bundle.js")
+
+module.exports = exports = (filename) => {
+  return webpack({
+    entry: filename,
+    context: __dirname,
+    module: {
+      loaders: [
+        {
+          test: /.scss$/,
+          loader: pathToLoader,
+        }
+      ],
+    },
+    output: {
+      path: path.join(__dirname, 'output'),
+      filename: 'test.bundle.js',
+      libraryTarget: 'commonjs2',
+    },
+  })
+  .then(stats => {
+    if (stats.hasErrors()) {
+      stats.compilation.errors[0].stats = stats;
+      throw stats.compilation.errors[0];
+    }
+    if (stats.hasWarnings()) {
+      stats.compilation.warnings[0].stats = stats;
+      throw stats.compilation.warnings[0];
+    }
+
+    delete require.cache[pathToTestBundle];
+    const compiled = require(pathToTestBundle);
+
+    return { compiled, stats };
+  });
+};
